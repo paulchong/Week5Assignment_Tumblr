3659f2211ef7505582436fde05b9e25296ff43e4
Paul Chong <paulchong.nz@gmail.com>
2015-03-08 22:02:30 -0700

    finalPush

--- Week5Assignment_Tumblr/ComposeViewController.swift
+++ Week5Assignment_Tumblr/ComposeViewController.swift
@@ -12,6 +12,14 @@ class ComposeViewController: UIViewController, UIViewControllerTransitioningDele
     
     var isPresenting: Bool = true
 
+    @IBOutlet weak var photoImage: UIImageView!
+    @IBOutlet weak var quoteImage: UIImageView!
+    @IBOutlet weak var videoImage: UIImageView!
+    @IBOutlet weak var linkImage: UIImageView!
+    @IBOutlet weak var chatImage: UIImageView!
+    @IBOutlet weak var textImage: UIImageView!
+    
+    
     
     @IBOutlet var tapGestureRecognizer: UITapGestureRecognizer!
     
@@ -23,8 +31,37 @@ class ComposeViewController: UIViewController, UIViewControllerTransitioningDele
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        photoImage.hidden = true
+        super.viewDidLoad()
+        delay(1) {
+            self.photoImage.hidden = false
+        }
+        
+        quoteImage.hidden = true
+        super.viewDidLoad()
+        delay(0.5) {
+            self.quoteImage.hidden = false
+        }
 
-        // Do any additional setup after loading the view.
+        textImage.hidden = true
+        super.viewDidLoad()
+        delay(0.3) {
+            self.textImage.hidden = false
+        }
+        
+        linkImage.hidden = true
+        super.viewDidLoad()
+        delay(0.8) {
+            self.linkImage.hidden = false
+        }
+        
+        videoImage.hidden = true
+        super.viewDidLoad()
+        delay(1.2) {
+            self.videoImage.hidden = false
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,7 +69,15 @@ class ComposeViewController: UIViewController, UIViewControllerTransitioningDele
         // Dispose of any resources that can be recreated.
     }
     
-
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
+    
     @IBAction func didTapOnNevermind(sender: UITapGestureRecognizer) {
         println("registering tap")
         dismissViewControllerAnimated(true, completion: nil)
@@ -72,6 +117,7 @@ class ComposeViewController: UIViewController, UIViewControllerTransitioningDele
             toViewController.view.alpha = 0
             UIView.animateWithDuration(0.4, animations: { () -> Void in
                 toViewController.view.alpha = 1
+                
                 }) { (finished: Bool) -> Void in
                     transitionContext.completeTransition(true)
             }
@@ -86,3 +132,7 @@ class ComposeViewController: UIViewController, UIViewControllerTransitioningDele
     }
     
 }
+
+//  NEXT STEP:  Already setup the animated transitions between tabBarVC and composeVC.  Now need to create the animations with the circles.
+
+
--- Week5Assignment_Tumblr/SearchViewController.swift
+++ Week5Assignment_Tumblr/SearchViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    
+    @IBOutlet weak var searchScrollView: UIScrollView!
+    @IBOutlet weak var searchImage: UIImageView!
+    @IBOutlet weak var loadingImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchScrollView.contentSize = searchImage.frame.size
 
+        super.viewDidLoad()
+        searchImage.hidden = true
+        super.viewDidLoad()
+        delay(3) {
+            self.searchImage.hidden = false
+        }
+        
+        var images = UIImage.animatedImageNamed("loading-", duration: 0.7)
+        loadingImage.image = images
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +37,14 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
 
     /*
     // MARK: - Navigation
